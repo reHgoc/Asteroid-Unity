58386d21942b75cd744523d51449d22f2c4efb24
reHgoc <spark1993g@mail.ru>
2022-02-02 15:22:16 +0100

    Move with slice
    need make stop move function

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -1,12 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    float SpeedShip = 3f;
-    public void Moving()
+    float SpeedShip;
+    float ChangeSpeed = 0.25f;
+    Vector2 vertical = Vector2.zero;
+    PlayerControls controls;
+
+    void Awake()
     {
-        transform.Translate(Vector3.up * SpeedShip * Time.fixedDeltaTime);
+        controls = new PlayerControls();
+
+        controls.Player.Move.performed += context => vertical = context.ReadValue<Vector2>();
+        controls.Player.Move.canceled += context => vertical = Vector2.zero;
+    }
+
+    private void Start()
+    {
+        SpeedShip = Mathf.Clamp(SpeedShip, .3f, 3f);
+    }
+
+  
+
+     void Movement()
+    {
+        Vector2 currentPosition = gameObject.transform.position;
+        SpeedShip += Time.fixedDeltaTime * ChangeSpeed;
+        Vector2 newPos = currentPosition + (vertical * SpeedShip);
+        transform.Translate(newPos * Time.fixedDeltaTime);
+
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+
+     void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+     void OnDisable()
+    {
+        controls.Player.Disable();
     }
 }
